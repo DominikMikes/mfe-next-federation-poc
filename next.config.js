1ca1ff39cdcb47c6876dafb3eed8a5b49d9b735f
dominik mikes <dominik.blau@gmail.com>
2023-06-10 10:48:51 +0200

    initial next setup

--- next.config.js
+++ next.config.js
@@ -1,4 +1,31 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+// const nextConfig = {}
 
-module.exports = nextConfig
+// module.exports = nextConfig
+
+const NextFederationPlugin = require('@module-federation/nextjs-mf');
+
+module.exports = {
+  webpack(config, options) {
+    const { isServer } = options;
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'MfeNext',
+        remotes: {
+        //   next1: `next1@http://localhost:3001/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
+        },
+        filename: 'static/chunks/remoteEntry.js',
+        exposes: {
+          './MfeNext': './src/app/page',
+        },
+        shared: {
+          // whatever else
+        },
+      })
+    );
+
+    return config;
+  },
+};
+
+import '@module-federation/nextjs-mf/src/include-defaults';
