18fc2bdc20474b5adab5854e054c3ea8b2a9187e
dominik mikes <dominik.blau@gmail.com>
2023-06-10 11:50:51 +0200

    fixed next federation config

--- next.config.js
+++ next.config.js
@@ -1,31 +1,26 @@
 /** @type {import('next').NextConfig} */
 // const nextConfig = {}
 
-// module.exports = nextConfig
-
 const NextFederationPlugin = require('@module-federation/nextjs-mf');
 
 module.exports = {
-  webpack(config, options) {
-    const { isServer } = options;
-    config.plugins.push(
-      new NextFederationPlugin({
-        name: 'MfeNext',
-        remotes: {
-        //   next1: `next1@http://localhost:3001/_next/static/${isServer ? 'ssr' : 'chunks'}/remoteEntry.js`,
-        },
-        filename: 'static/chunks/remoteEntry.js',
-        exposes: {
-          './MfeNext': './src/app/page',
-        },
-        shared: {
-          // whatever else
-        },
-      })
-    );
+  webpack(config, options) {    
+    if (!options.isServer) {
+      config.plugins.push(
+        new NextFederationPlugin({
+          name: 'MfeNext',
+          remotes: {
+          //   MfeReact: `MfeReact@http://localhost:3001/remoteEntry.js`,
+          },
+          filename: 'static/chunks/remoteEntry.js',
+          exposes: {
+            './MfeNext': './src/app/page',
+          },
+          shared: {},
+        })
+      );
+    }
 
     return config;
   },
 };
-
-import '@module-federation/nextjs-mf/src/include-defaults';
